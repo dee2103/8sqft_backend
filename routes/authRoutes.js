39d21ac9e33b473bed22ca9e438588cb7b23ffdf
dee2103 <dee.wab.2103@gmail.com>
2024-12-13 12:01:51 +0530

    update registration with the otp hanges

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 
 import express from "express";
-import { sendOtpToMobile, verifyMobileOtp, sendOtpToEmail, verifyEmailOtp } from "../controllers/authController.js";
+import { sendOtpToMobile, verifyMobileOtp, sendOtpToEmail, verifyEmailOtp, resendOtpToEmail, registerUser } from "../controllers/authController.js";
 
 
 
@@ -13,5 +13,7 @@ router.post("/verify-otp-mobile", verifyMobileOtp);
 
 router.post("/send-otp-email", sendOtpToEmail);
 router.post("/verify-otp-email", verifyEmailOtp);
+router.post("/resend-otp-email", resendOtpToEmail);
+router.post("/register-user", registerUser);
 
 export default router;
